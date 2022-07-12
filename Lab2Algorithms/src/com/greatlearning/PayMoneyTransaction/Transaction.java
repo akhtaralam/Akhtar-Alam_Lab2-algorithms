66af213b8be103255f4de43b03dfb23d504c5aab
Akhtar Alam <59148622+akhtaralam@users.noreply.github.com>
2022-07-12 21:53:58 +0530

    Initial commit

---
+++ Lab2Algorithms/src/com/greatlearning/PayMoneyTransaction/Transaction.java
@@ -0,0 +1,56 @@
+package com.greatlearning.PayMoneyTransaction;
+
+import java.util.Scanner;
+
+public class Transaction {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the size of transaction array");
+
+		int size = sc.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter the values of array");
+
+		for (int i = 0; i < size; i++)
+			arr[i] = sc.nextInt();
+
+		System.out.println("Enter the total number of targets that needs to be achieved");
+
+		int numOfTargetNeedsToCheck = sc.nextInt();
+
+		while (numOfTargetNeedsToCheck-- != 0) {
+
+			int flag = 0;
+
+			long target;
+
+			System.out.println("Enter the value of target");
+
+			target = sc.nextInt();
+
+			long sum = 0;
+
+			for (int i = 0; i < size; i++) {
+				sum += arr[i];
+
+				if (sum >= target) {
+					System.out.println("Target achieved after " + (i + 1) + " transactions");
+					flag = 1;
+					break;
+				}
+			}
+
+			if (flag == 0) {
+				System.out.println("Given target is not achieved");
+			}
+
+		}
+
+	}
+
+}
